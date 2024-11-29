3c03a16dde5e71d46526b927508428be644e1bd0
kevin-rojas17 <kevin.rojascuritima@unas.edu.pe>
2024-11-29 16:00:04 -0500

    Primer commit

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -3,11 +3,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const port = 5000;
 
-// Rutas para enrutar a los microservicios
 app.use('/public', createProxyMiddleware({ target: 'http://localhost:3000', changeOrigin: true }));
 app.use('/private', createProxyMiddleware({ target: 'http://localhost:4000', changeOrigin: true }));
 
-// Iniciar servidor de API Gateway
 app.listen(port, () => {
   console.log(`API Gateway escuchando en http://localhost:${port}`);
 });
--- microservice-public/index.js
+++ microservice-public/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-// Cadena de conexión a MongoDB
+
 const dbURI = "mongodb+srv://TOMut4mn3tPvITie:TOMut4mn3tPvITie@comedor.zhardxq.mongodb.net/?retryWrites=true&w=majority&appName=comedor";
 mongoose.connect(dbURI)
   .then(() => console.log("Conectado a MongoDB"))
@@ -16,16 +16,16 @@ const bookSchema = new mongoose.Schema({
   genre: String
 });
 
-// Crear el modelo de libro
+
 const Book = mongoose.model('Book', bookSchema);
 
-// Ruta pública para obtener libros
+
 app.get('/books', async (req, res) => {
   try {
-    const books = await Book.find(); // Esperamos a que se resuelva la promesa
-    res.json(books); // Respondemos con los libros en formato JSON
+    const books = await Book.find(); 
+    res.json(books); 
   } catch (err) {
-    res.status(500).send("Error en la base de datos."); // En caso de error, respondemos con un mensaje de error
+    res.status(500).send("Error en la base de datos."); 
   }
 });
 
